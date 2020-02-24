66ca9367756682355fc2c88e99e99458542624d2
roberto cortes <robertcortesgrimm223@gmail.com>
2020-02-24 10:35:07 -0600

    setup controller created user model

--- app.js
+++ app.js
@@ -3,34 +3,52 @@ require('./db/db.js')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-
+const cors = require('cors')
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 
 PORT = process.env.PORT
 
-server.listen(80)
-
-// console.log(server);
-
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html')
-})
-
-io.on('connection', function(socket) {
-	socket.emit('news', {hello: 'World'})
-	socket.on('my other event', function(data) {
-		console.log('here is my data on app.js', data);
-	})
-})
-
-
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(bodyParser.json())
 
+app.use(cors())
 
+// server.listen(80)
+
+
+
+
+// app.get('/', function(req, res) {
+// 	res.sendFile(__dirname + '/index.html')
+// })
+
+// io.on('connection', function(socket) {
+// 	socket.emit('news', {hello: 'World'})
+// 	socket.on('my other event', function(data) {
+// 		console.log('here is my data on app.js', data);
+// 	})
+// })
+
+// io.on('connection', (socket)=>{
+// 	io.emit('this', {will: 'be received by everyone'});
+
+// 	socket.on('private message', (frm, msg) => {
+// 		console.log(`I have received a private message by, ${frm}, saying, ${msg}`);
+// 	})
+
+// 	socket.on('disconnect', ()=>{
+// 		io.emit('user disconnected')
+// 	})
+// })
+
+
+
+app.get('/', (req,res)=>{
+	res.send('Test route works')
+})
 
 
 
---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	firstName: {
+		type: String,
+		required: true
+	}
+	lastName: {
+		type: String,
+		required: true
+	}
+	dob: {
+		type: Date,
+		required: true
+	}
+	profilePhoto: {
+		type: Buffer,
+		contentType: String,
+		default: ''
+	}
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
