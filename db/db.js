11081d95245bca21725040cb16fcb0dc0d9cd719
roberto cortes <robertcortesgrimm223@gmail.com>
2020-03-04 09:54:14 -0600

    fixed db connection

--- db/db.js
+++ db/db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
 
-
-
-mongoose.connect('mongodb://localhost:27017/sou-app', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sou-app', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: false
